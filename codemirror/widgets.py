5662b003ab1df836f2e090ce13162d5074f31e52
Pablo Martín <goinnn@gmail.com>
2012-10-04 13:25:01 +0200

    Use the django simplejson, pep8 and pyflakes

--- codemirror/widgets.py
+++ codemirror/widgets.py
@@ -3,25 +3,25 @@
 # Created:    2010/09/09
 # Author:         alisue
 #
-import simplejson as json
 from django import forms
 from django.conf import settings
-from django.template.loader import render_to_string
 from django.contrib.admin.widgets import AdminTextareaWidget
+from django.template.loader import render_to_string
+from django.utils import simplejson as json
 from django.utils.safestring import mark_safe
-from django.core.exceptions import ImproperlyConfigured
 
-    
+
 # set default settings
-CODEMIRROR_PATH  = getattr(settings, 'CODEMIRROR_PATH', 'codemirror')
+CODEMIRROR_PATH = getattr(settings, 'CODEMIRROR_PATH', 'codemirror')
 if CODEMIRROR_PATH.endswith('/'):
     CODEMIRROR_PATH = CODEMIRROR_PATH[:-1]
-CODEMIRROR_DEFAULT_PARSERFILE  = getattr(settings, 'CODEMIRROR_DEFAULT_PARSERFILE', 'parsedummy.js')
-CODEMIRROR_DEFAULT_STYLESHEET  = getattr(settings, 'CODEMIRROR_DEFAULT_STYLESHEET', '')
+CODEMIRROR_DEFAULT_PARSERFILE = getattr(settings, 'CODEMIRROR_DEFAULT_PARSERFILE', 'parsedummy.js')
+CODEMIRROR_DEFAULT_STYLESHEET = getattr(settings, 'CODEMIRROR_DEFAULT_STYLESHEET', '')
+
 
 class CodeMirrorTextarea(forms.Textarea):
     u"""Textarea widget render with `CodeMirror`
-    
+
     CodeMirror:
         http://codemirror.net/
     """
@@ -31,15 +31,15 @@ class CodeMirrorTextarea(forms.Textarea):
         js = (
             r"%s/js/codemirror.js" % CODEMIRROR_PATH,
         )
-        
+
     def __init__(self, attrs=None, path=None, parserfile=None, stylesheet=None, **kwargs):
         u"""Constructor of CodeMirrorTextarea
-        
+
         Attribute:
             path          - CodeMirror directory URI (DEFAULT = settings.CODEMIRROR_PATH)
             parserfile    - CodeMirror parserfile attribute (string or string array: DEFAULT = settings.CODEMIRROR_DEFAULT_PARSERFILE)
             stylesheet    - CodeMirror stylesheet attribute (uri or uri array: DEFAULT = settings.CODEMIRROR_DEFAULT_STYLESHEET)
-        
+
         Example:
             *-------------------------------*
             + static
@@ -51,7 +51,7 @@ class CodeMirrorTextarea(forms.Textarea):
                   - parsexml.js
             *-------------------------------*
             CODEMIRROR_PATH = r"codemirror"
-            
+
             codemirror = CodeMirrorTextarea(
                 # parserfile='parsexml.js',                                # Can be written as the left when only one file is needed.
                 parserfile=['parsexml.js'],
@@ -69,7 +69,7 @@ class CodeMirrorTextarea(forms.Textarea):
             self.parserfile = (self.parserfile,)
         if not hasattr(self.stylesheet, '__iter__'):
             self.stylesheet = (self.stylesheet,)
-    
+
     def render(self, name, value, attrs=None):
         u"""Render CodeMirrorTextarea"""
         html = super(CodeMirrorTextarea, self).render(name, value, attrs)
@@ -84,10 +84,12 @@ class CodeMirrorTextarea(forms.Textarea):
         code = render_to_string(r"codemirror/javascript.html", kwargs)
         body = "%s\n%s" % (html, code)
         return mark_safe(body)
-     
+
+
 class AdminCodeMirrorTextareaWidget(CodeMirrorTextarea, AdminTextareaWidget):
     u"""CodeMirrorTextarea for Admin site"""
     pass
+
 
 class AdminHTMLEditor(AdminCodeMirrorTextareaWidget):
     def __init__(self, *args, **kwargs):
