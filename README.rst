b42a5dd88c8416997eff499bf5b6024b6a97f561
Alan Lu <gotoalanlu@gmail.com>
2013-01-19 01:55:35 -0600

    Added all other options for CodeMirror except keymaps.

--- README.rst
+++ README.rst
@@ -16,20 +16,29 @@ How to Use
 
 1.	First, you need to specified ``CODEMIRROR_PATH`` on ``settings.py``.
 	``CODEMIRROR_PATH`` is the URI of CodeMirror directory like ``CODEMIRROR_PATH = r"javascript/codemirror"``.
-	If you don't specify it, it defaults to ``'codemirror'``
+	If you don't specify it, it defaults to ``'codemirror'``.
+	CodeMirror should be put there.
 2.	Use ``codemirror.widgets.CodeMirrorTextarea`` widgets for target Textarea like below::
 	
 		from django import forms
 		from codemirror.widgets import CodeMirrorTextarea
 
-		codemirror = CodeMirrorTextarea(mode="javascript")
+		codemirror = CodeMirrorTextarea(mode="python", theme="cobalt", config={ 'fixedGutter': True })
 		document = forms.TextField(widget=codemirror)
+
 
 Settings
 =========================================
+
 ``CODEMIRROR_PATH``
     the URI of CodeMirror directory (your CodeMirror installation should live in ``{{ STATIC_URL }}/{{ CODEMIRROR_PATH }}``)
 
 ``CODEMIRROR_MODE``
 	the default mode which may be a string or configuration map (DEFAULT: 'javascript')
+
+``CODEMIRROR_THEME``
+    the default theme applied (DEFAULT: 'default')
+
+``CODEMIRROR_CONFIG``
+    base mapping for the rest of the CodeMirror options (DEFAULT: ``{ 'lineNumbers': True }``)
 
--- codemirror/widgets.py
+++ codemirror/widgets.py
@@ -3,7 +3,8 @@
 # Created:    2010/09/09
 # Author:         alisue
 #
-import json
+import json, re
+from itertools import chain
 from django import forms
 from django.conf import settings
 from django.contrib.admin.widgets import AdminTextareaWidget
@@ -14,6 +15,10 @@ CODEMIRROR_PATH = getattr(settings, 'CODEMIRROR_PATH', 'codemirror')
 if CODEMIRROR_PATH.endswith('/'):
     CODEMIRROR_PATH = CODEMIRROR_PATH[:-1]
 CODEMIRROR_MODE = getattr(settings, 'CODEMIRROR_MODE', 'javascript')
+CODEMIRROR_THEME = getattr(settings, 'CODEMIRROR_THEME', 'default')
+CODEMIRROR_CONFIG = getattr(settings, 'CODEMIRROR_CONFIG', { 'lineNumbers': True })
+
+THEME_CSS_FILENAME_RE = re.compile(r'[\w-]+')
 
 class CodeMirrorTextarea(forms.Textarea):
     u"""Textarea widget render with `CodeMirror`
@@ -24,21 +29,29 @@ class CodeMirrorTextarea(forms.Textarea):
     
     @property
     def media(self):
-        mode_name = self.mode['name']
+        mode_name = self.mode_name
         return forms.Media(css = {
-                'all': ("%s/lib/codemirror.css" % CODEMIRROR_PATH,),
+                'all': ("%s/lib/codemirror.css" % CODEMIRROR_PATH,) +
+                    tuple("%s/theme/%s.css" % (CODEMIRROR_PATH, theme_css_filename)
+                        for theme_css_filename in self.theme_css),
             },
             js = (
                 "%s/lib/codemirror.js" % CODEMIRROR_PATH,
                 "%s/mode/%s/%s.js" % (CODEMIRROR_PATH, mode_name, mode_name),
             ))
     
-    def __init__(self, attrs=None, mode=None, **kwargs):
+    def __init__(self, attrs=None, mode=None, theme=None, config=None, **kwargs):
         u"""Constructor of CodeMirrorTextarea
 
         Attribute:
             path          - CodeMirror directory URI (DEFAULT = settings.CODEMIRROR_PATH)
-            mode          - Name of language or a configuration object as described in CodeMirror docs (DEFAULT = settings.CODEMIRROR_MODE)
+            mode          - Name of language or a modal configuration object as described in CodeMirror docs.
+                            Used to autoload an appropriate language plugin js file according to filename conventions.
+                            (DEFAULT = settings.CODEMIRROR_MODE)
+            theme         - Name of theme. Also autoloads theme plugin css according to filename conventions.
+                            (DEFAULT = settings.CODEMIRROR_THEME)
+            config        - The rest of the options passed into CodeMirror as a python map.
+                            (updated from settings.CODEMIRROR_CONFIG)
 
         Example:
             *-------------------------------*
@@ -46,24 +59,42 @@ class CodeMirrorTextarea(forms.Textarea):
               + codemirror
                 + lib
                   - codemirror.js
+                  - codemirror.css
                 + mode
                   + python
                     - python.js
+                + theme
+                  + cobalt.css
             *-------------------------------*
-            CODEMIRROR_PATH = r"codemirror"
+            CODEMIRROR_PATH = "codemirror"
 
-            codemirror = CodeMirrorTextarea(mode="python")
+            codemirror = CodeMirrorTextarea(mode="python", theme="cobalt", config={ 'fixedGutter': True })
             document = forms.TextField(widget=codemirror)
         """
         super(CodeMirrorTextarea, self).__init__(attrs=attrs, **kwargs)
+        
         mode = mode or CODEMIRROR_MODE
         if isinstance(mode, basestring):
             mode = { 'name': mode }
-        self.mode = mode
+        self.mode_name = mode['name']
+        
+        theme = theme or CODEMIRROR_THEME
+        theme_css_filename = THEME_CSS_FILENAME_RE.search(theme).group(0)
+        if theme_css_filename == 'default':
+            self.theme_css = []
+        else:
+            self.theme_css = [theme_css_filename]
+        
+        config = config or {}
+        self.option_json = json.dumps(dict(chain(
+            CODEMIRROR_CONFIG.items(),
+            config.items(),
+            [('mode', mode), ('theme', theme)])))
     
     def render(self, name, value, attrs=None):
         u"""Render CodeMirrorTextarea"""
         output = [super(CodeMirrorTextarea, self).render(name, value, attrs),
-            '<script type="text/javascript">CodeMirror.fromTextArea(document.getElementById(%s), { mode: %s });</script>' % ('"id_%s"' % name, json.dumps(self.mode))]
+            '<script type="text/javascript">CodeMirror.fromTextArea(document.getElementById(%s), %s);</script>' %
+                ('"id_%s"' % name, self.option_json)]
         return mark_safe('\n'.join(output))
 
