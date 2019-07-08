4495bc55d69912444fbdf77ee85bd49c014c2dfc
Andreas Kloeckner <inform@tiker.net>
2019-07-08 15:12:17 -0500

    Add renderer kwarg to CodeMirrorTextarea.renderer for Django 2.1+ compatibility

--- codemirror/widgets.py
+++ codemirror/widgets.py
@@ -159,13 +159,14 @@ class CodeMirrorTextarea(forms.Textarea):
             config.items(),
             [('mode', mode), ('theme', theme)])))
 
-    def render(self, name, value, attrs=None):
+    def render(self, name, value, attrs=None, renderer=None):
         u"""Render CodeMirrorTextarea"""
         if self.js_var_format is not None:
             js_var_bit = 'var %s = ' % (self.js_var_format % name)
         else:
             js_var_bit = ''
-        output = [super(CodeMirrorTextarea, self).render(name, value, attrs),
+        output = [super(CodeMirrorTextarea, self).render(
+                        name, value, attrs, renderer),
             '<script type="text/javascript">%sCodeMirror.fromTextArea(document.getElementById(%s), %s);</script>' %
                 (js_var_bit, '"id_%s"' % name, self.option_json)]
         return mark_safe('\n'.join(output))
